aeeeb143b0f57c6d4ba58ae3c6e3d17f9547bcee
Laurent VAYLET <laurent.vaylet@gmail.com>
2022-09-29 18:26:58 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[Let's build a CLI in Go with Cobra](https://www.thorsten-hans.com/lets-build-a-cli-in-go-with-cobra/)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/lvaylet/stringer/cmd/stringer"
+
+func main() {
+	stringer.Execute()
+}
